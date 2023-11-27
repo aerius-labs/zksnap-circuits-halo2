cf73c16377f1ff8557c8ecf0a68aaa135ed78b0a
vishal <vishalkoolkarni0045@gmail.com>
2023-11-27 11:35:35 +0530

    feat: merkle-tree chip and test

---
+++ src/chips/merkle_tree_chip.rs
@@ -0,0 +1,173 @@
+use super::poseidon::hash::{PoseidonChip, PoseidonConfig};
+use super::poseidon::spec::MySpec;
+use halo2_proofs::{arithmetic::FieldExt, circuit::*, plonk::*, poly::Rotation};
+
+const WIDTH: usize = 3;
+const RATE: usize = 2;
+const L: usize = 2;
+
+#[derive(Debug, Clone)]
+pub struct MerkleTreeV3Config<F: FieldExt> {
+    pub advice: [Column<Advice>; 3],
+    pub bool_selector: Selector,
+    pub swap_selector: Selector,
+    pub instance: Column<Instance>,
+    pub poseidon_config: PoseidonConfig<F, WIDTH, RATE, L>,
+}
+#[derive(Debug, Clone)]
+pub struct MerkleTreeV3Chip<F: FieldExt> {
+    config: MerkleTreeV3Config<F>,
+}
+
+impl<F: FieldExt> MerkleTreeV3Chip<F> {
+    pub fn construct(config: MerkleTreeV3Config<F>) -> Self {
+        Self { config }
+    }
+
+    pub fn configure(
+        meta: &mut ConstraintSystem<F>,
+        advice: [Column<Advice>; 3],
+        instance: Column<Instance>,
+    ) -> MerkleTreeV3Config<F> {
+        let col_a = advice[0];
+        let col_b = advice[1];
+        let col_c = advice[2];
+
+        // create selectors
+        let bool_selector = meta.selector();
+        let swap_selector = meta.selector();
+
+        meta.enable_equality(col_a);
+        meta.enable_equality(col_b);
+        meta.enable_equality(col_c);
+        meta.enable_equality(instance);
+
+        // Enforces that c is either a 0 or 1 when the bool selector is enabled
+        // s * c * (1 - c) = 0
+        meta.create_gate("bool constraint", |meta| {
+            let s = meta.query_selector(bool_selector);
+            let c = meta.query_advice(col_c, Rotation::cur());
+            vec![s * c.clone() * (Expression::Constant(F::from(1)) - c)]
+        });
+
+        // Enforces that if the swap bit (c) is on, l=b and r=a. Otherwise, l=a and r=b.
+        // s * (c * 2 * (b - a) - (l - a) - (b - r)) = 0
+        // This applies only when the swap selector is enabled
+        meta.create_gate("swap constraint", |meta| {
+            let s = meta.query_selector(swap_selector);
+            let a = meta.query_advice(col_a, Rotation::cur());
+            let b = meta.query_advice(col_b, Rotation::cur());
+            let c = meta.query_advice(col_c, Rotation::cur());
+            let l = meta.query_advice(col_a, Rotation::next());
+            let r = meta.query_advice(col_b, Rotation::next());
+            vec![
+                s * (c * Expression::Constant(F::from(2)) * (b.clone() - a.clone())
+                    - (l - a)
+                    - (b - r)),
+            ]
+        });
+
+        let hash_inputs = (0..WIDTH).map(|_| meta.advice_column()).collect::<Vec<_>>();
+
+        let poseidon_config =
+            PoseidonChip::<F, MySpec<F, WIDTH, RATE>, WIDTH, RATE, L>::configure(meta, hash_inputs);
+
+        MerkleTreeV3Config {
+            advice: [col_a, col_b, col_c],
+            bool_selector,
+            swap_selector,
+            instance,
+            poseidon_config,
+        }
+    }
+
+    pub fn assing_leaf(
+        &self,
+        mut layouter: impl Layouter<F>,
+        leaf: Value<F>,
+    ) -> Result<AssignedCell<F, F>, Error> {
+        let node_cell = layouter.assign_region(
+            || "assign leaf",
+            |mut region| region.assign_advice(|| "assign leaf", self.config.advice[0], 0, || leaf),
+        )?;
+
+        Ok(node_cell)
+    }
+
+    pub fn merkle_prove_layer(
+        &self,
+        mut layouter: impl Layouter<F>,
+        node_cell: &AssignedCell<F, F>,
+        path_element: Value<F>,
+        index: Value<F>,
+    ) -> Result<AssignedCell<F, F>, Error> {
+        let (left, right) = layouter.assign_region(
+            || "merkle prove layer",
+            |mut region| {
+                // Row 0
+                self.config.bool_selector.enable(&mut region, 0)?;
+                self.config.swap_selector.enable(&mut region, 0)?;
+                node_cell.copy_advice(
+                    || "copy node cell from previous prove layer",
+                    &mut region,
+                    self.config.advice[0],
+                    0,
+                )?;
+                region.assign_advice(
+                    || "assign element",
+                    self.config.advice[1],
+                    0,
+                    || path_element,
+                )?;
+                region.assign_advice(|| "assign index", self.config.advice[2], 0, || index)?;
+
+                // Row 1
+                // Here we just perform the assignment - no hashing is performed here!
+                let node_cell_value = node_cell.value().map(|x| x.to_owned());
+                let (mut l, mut r) = (node_cell_value, path_element);
+                index.map(|x| {
+                    (l, r) = if x == F::zero() { (l, r) } else { (r, l) };
+                });
+
+                // We need to perform the assignment of the row below in order to perform the swap check
+                let left = region.assign_advice(
+                    || "assign left to be hashed",
+                    self.config.advice[0],
+                    1,
+                    || l,
+                )?;
+                let right = region.assign_advice(
+                    || "assign right to be hashed",
+                    self.config.advice[1],
+                    1,
+                    || r,
+                )?;
+
+                Ok((left, right))
+            },
+        )?;
+
+        // instantiate the poseidon_chip
+        let poseidon_chip = PoseidonChip::<F, MySpec<F, WIDTH, RATE>, WIDTH, RATE, L>::construct(
+            self.config.poseidon_config.clone(),
+        );
+
+        // The hash function inside the poseidon_chip performs the following action
+        // 1. Copy the left and right cells from the previous row
+        // 2. Perform the hash function and assign the digest to the current row
+        // 3. Constrain the digest to be equal to the hash of the left and right values
+        let digest =
+            poseidon_chip.hash(layouter.namespace(|| "hash row constaint"), [left, right])?;
+        Ok(digest)
+    }
+
+    // Enforce permutation check between input cell and instance column at row passed as input
+    pub fn expose_public(
+        &self,
+        mut layouter: impl Layouter<F>,
+        cell: &AssignedCell<F, F>,
+        row: usize,
+    ) -> Result<(), Error> {
+        layouter.constrain_instance(cell.cell(), self.config.instance, row)
+    }
+}
---
+++ src/circuits/merkle_tree_circuit.rs
@@ -0,0 +1,160 @@
+use super::super::chips::merkle_tree_chip::{MerkleTreeV3Chip, MerkleTreeV3Config};
+use halo2_proofs::{arithmetic::FieldExt, circuit::*, plonk::*};
+
+#[derive(Default)]
+pub struct MerkleTreeV3Circuit<F: FieldExt> {
+    pub leaf: Value<F>,
+    pub path_elements: Vec<Value<F>>,
+    pub path_indices: Vec<Value<F>>,
+}
+
+impl<F: FieldExt> Circuit<F> for MerkleTreeV3Circuit<F> {
+    type Config = MerkleTreeV3Config<F>;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        // config for the merkle tree chip
+        let col_a = meta.advice_column();
+        let col_b = meta.advice_column();
+        let col_c = meta.advice_column();
+        let instance = meta.instance_column();
+
+        MerkleTreeV3Chip::configure(meta, [col_a, col_b, col_c], instance)
+    }
+
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(), Error> {
+        let chip = MerkleTreeV3Chip::construct(config);
+        let leaf_cell = chip.assing_leaf(layouter.namespace(|| "assign leaf"), self.leaf)?;
+        chip.expose_public(layouter.namespace(|| "public leaf"), &leaf_cell, 0)?;
+
+        // apply it for level 0 of the merkle tree
+        // node cell passed as input is the leaf cell
+        let mut digest = chip.merkle_prove_layer(
+            layouter.namespace(|| "merkle_prove"),
+            &leaf_cell,
+            self.path_elements[0],
+            self.path_indices[0],
+        )?;
+
+        // apply it for the remaining levels of the merkle tree
+        // node cell passed as input is the digest cell
+        for i in 1..self.path_elements.len() {
+            digest = chip.merkle_prove_layer(
+                layouter.namespace(|| "next level"),
+                &digest,
+                self.path_elements[i],
+                self.path_indices[i],
+            )?;
+        }
+        chip.expose_public(layouter.namespace(|| "public root"), &digest, 1)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MerkleTreeV3Circuit;
+    use halo2_gadgets::poseidon::primitives::{self as poseidon, ConstantLength, P128Pow5T3};
+    use halo2_proofs::{circuit::Value, dev::MockProver, halo2curves::pasta::Fp};
+
+    const WIDTH: usize = 3;
+    const RATE: usize = 2;
+    const L: usize = 2;
+
+    fn compute_merkle_root(leaf: &u64, elements: &Vec<u64>, indices: &Vec<u64>) -> Fp {
+        let k = elements.len();
+        let mut digest = Fp::from(leaf.clone());
+        let mut message: [Fp; 2];
+        for i in 0..k {
+            if indices[i] == 0 {
+                message = [digest, Fp::from(elements[i])];
+            } else {
+                message = [Fp::from(elements[i]), digest];
+            }
+
+            digest = poseidon::Hash::<_, P128Pow5T3, ConstantLength<L>, WIDTH, RATE>::init()
+                .hash(message);
+        }
+        return digest;
+    }
+
+    #[test]
+    fn test_merkle_tree() {
+        let leaf = 99u64;
+        let elements = vec![5u64; (2u64.pow(8)) as usize];
+        let indices = vec![0u64; (2u64.pow(8)) as usize];
+
+        let root = compute_merkle_root(&leaf, &elements, &indices);
+
+        let leaf_fp = Value::known(Fp::from(leaf));
+        let elements_fp: Vec<Value<Fp>> = elements
+            .iter()
+            .map(|x| Value::known(Fp::from(x.to_owned())))
+            .collect();
+        let indices_fp: Vec<Value<Fp>> = indices
+            .iter()
+            .map(|x| Value::known(Fp::from(x.to_owned())))
+            .collect();
+
+        let circuit = MerkleTreeV3Circuit {
+            leaf: leaf_fp,
+            path_elements: elements_fp,
+            path_indices: indices_fp,
+        };
+
+        let correct_public_input = vec![Fp::from(leaf), root];
+        let valid_prover = MockProver::run(20, &circuit, vec![correct_public_input]).unwrap();
+        valid_prover.assert_satisfied();
+        println!("2^8 leafs");
+        let wrong_public_input = vec![Fp::from(leaf), Fp::from(0)];
+        let invalid_prover = MockProver::run(20, &circuit, vec![wrong_public_input]).unwrap();
+        assert!(invalid_prover.verify().is_err());
+    }
+}
+
+#[cfg(feature = "dev-graph")]
+#[test]
+fn print_merkle_tree_3() {
+    use halo2_proofs::halo2curves::pasta::Fp;
+    use plotters::prelude::*;
+
+    let root =
+        BitMapBackend::new("prints/merkle-tree-3-layout.png", (1024, 3096)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let root = root
+        .titled("Merkle Tree 3 Layout", ("sans-serif", 60))
+        .unwrap();
+
+    let leaf = 99u64;
+    let elements = vec![1u64, 5u64, 6u64, 9u64, 9u64];
+    let indices = vec![0u64, 0u64, 0u64, 0u64, 0u64];
+    let digest: u64 = leaf + elements.iter().sum::<u64>();
+
+    let leaf_fp = Value::known(Fp::from(leaf));
+    let elements_fp: Vec<Value<Fp>> = elements
+        .iter()
+        .map(|x| Value::known(Fp::from(x.to_owned())))
+        .collect();
+    let indices_fp: Vec<Value<Fp>> = indices
+        .iter()
+        .map(|x| Value::known(Fp::from(x.to_owned())))
+        .collect();
+
+    let circuit = MerkleTreeV3Circuit {
+        leaf: leaf_fp,
+        path_elements: elements_fp,
+        path_indices: indices_fp,
+    };
+
+    halo2_proofs::dev::CircuitLayout::default()
+        .render(8, &circuit, &root)
+        .unwrap();
+}
