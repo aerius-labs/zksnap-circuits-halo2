6b2b632c1a5b976c3c4efc794106b9e0b6c198d1
shryasss <shreyas_londhe@outlook.com>
2023-12-29 12:50:06 +0530

    feat: added dummy circuit

--- src/aggregator/mod.rs
+++ src/aggregator/mod.rs
@@ -1,14 +1,36 @@
 use halo2_base::{
-    utils::BigPrimeField,
-    gates::circuit::builder::BaseCircuitBuilder,
+    utils::ScalarField,
+    gates::{ circuit::builder::BaseCircuitBuilder, GateChip, GateInstructions },
     AssignedValue,
 };
 
 #[derive(Clone, Debug)]
-pub struct CircuitInput {}
+struct CircuitInput {
+    a: u64,
+    b: u64,
+    c: u64,
+}
 
-fn circuit<F: BigPrimeField>(
+fn circuit<F: ScalarField>(
     builder: &mut BaseCircuitBuilder<F>,
     input: CircuitInput,
     make_public: &mut Vec<AssignedValue<F>>
-) {}
+) {
+    let ctx = builder.main(0);
+    let gate = GateChip::<F>::default();
+
+    let a = ctx.load_witness(F::from(input.a));
+    let b = ctx.load_witness(F::from(input.b));
+
+    let c = ctx.load_witness(F::from(input.c));
+    make_public.push(c.clone());
+
+    let a2 = gate.mul(ctx, a.clone(), a.clone());
+    let b2 = gate.mul(ctx, b.clone(), b.clone());
+    let res = gate.mul(ctx, a2, b2);
+
+    let is_equal = gate.is_equal(ctx, res, c);
+    assert_eq!(is_equal.value(), &F::from(1));
+}
+
+
