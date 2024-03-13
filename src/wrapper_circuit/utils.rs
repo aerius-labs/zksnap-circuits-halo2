026f65a76981c96e727e9b9df1494e7cd32f7ecd
vishal <vishalkoolkarni0045@gmail.com>
2024-03-13 18:56:02 +0530

    chore: refactor

--- src/wrapper_circuit/utils.rs
+++ src/wrapper_circuit/utils.rs
@@ -82,27 +82,31 @@ fn generate_voter_circuit_inputs(
     input
 }
 
-fn update_idx_leaf(leaves: Vec<IMTLeaf<Fr>>, new_val: Fr, new_val_idx: u64) -> (Vec<IMTLeaf<Fr>> ,usize){
+fn update_idx_leaf(
+    leaves: Vec<IMTLeaf<Fr>>,
+    new_val: Fr,
+    new_val_idx: u64,
+) -> (Vec<IMTLeaf<Fr>>, usize) {
     let mut idx_leaves = leaves.clone();
-    let mut low_leaf_idx=0;
+    let mut low_leaf_idx = 0;
     for (i, node) in leaves.iter().enumerate() {
         if node.next_val == Fr::zero() && i == 0 {
             idx_leaves[i + 1].val = new_val;
             idx_leaves[i].next_val = new_val;
             idx_leaves[i].next_idx = Fr::from(i as u64 + 1);
-            low_leaf_idx=i;
+            low_leaf_idx = i;
             break;
         }
-        if node.val < new_val && (node.next_val > new_val || node.next_val==Fr::zero()){
+        if node.val < new_val && (node.next_val > new_val || node.next_val == Fr::zero()) {
             idx_leaves[new_val_idx as usize].val = new_val;
             idx_leaves[new_val_idx as usize].next_val = idx_leaves[i].next_val;
             idx_leaves[new_val_idx as usize].next_idx = idx_leaves[i].next_idx;
             idx_leaves[i].next_val = new_val;
             idx_leaves[i].next_idx = Fr::from(new_val_idx);
-            low_leaf_idx=i;
+            low_leaf_idx = i;
         }
     }
-    (idx_leaves,low_leaf_idx)
+    (idx_leaves, low_leaf_idx)
 }
 
 fn generate_state_transition_circuit_inputs(
@@ -110,18 +114,21 @@ fn generate_state_transition_circuit_inputs(
     nullifier_affine: Secp256k1Affine,
     incoming_vote: Vec<BigUint>,
     prev_vote: Vec<BigUint>,
-    idx_tree_leaf:Vec<IMTLeaf<Fr>>,
-    new_val: Fr,
-    round:u64
-) -> StateTranInput<Fr> {
+    idx_tree_leaf: Vec<IMTLeaf<Fr>>,
+    round: u64,
+    leaves: Vec<Fr>,
+) -> (StateTranInput<Fr>, Vec<Fr>) {
     let tree_size = pow(2, 3);
-    let mut leaves = Vec::<Fr>::new();
+    let mut leaves = leaves.clone();
     let mut native_hasher = Poseidon::<Fr, T, RATE>::new(R_F, R_P);
 
     for i in 0..tree_size {
-            native_hasher.update(&[idx_tree_leaf[i].val, idx_tree_leaf[i].next_idx, idx_tree_leaf[i].next_val]);
-            leaves.push(native_hasher.squeeze_and_reset());
-        
+        native_hasher.update(&[
+            idx_tree_leaf[i].val,
+            idx_tree_leaf[i].next_idx,
+            idx_tree_leaf[i].next_val,
+        ]);
+        leaves.push(native_hasher.squeeze_and_reset());
     }
 
     let nullifier_compress = compress_native_nullifier(&nullifier_affine);
@@ -133,16 +140,20 @@ fn generate_state_transition_circuit_inputs(
 
     let old_root = tree.get_root();
 
-    
-    let (updated_idx_leaves,low_leaf_idx)=update_idx_leaf(idx_tree_leaf, new_val, round);
-    let low_leaf=idx_tree_leaf[low_leaf_idx];
+    let (updated_idx_leaves, low_leaf_idx) = update_idx_leaf(idx_tree_leaf.clone(), new_val, round);
+    let low_leaf = idx_tree_leaf[low_leaf_idx].clone();
     let (low_leaf_proof, low_leaf_proof_helper) = tree.get_proof(low_leaf_idx);
     assert_eq!(
-        tree.verify_proof(&leaves[low_leaf_idx], low_leaf_idx, &tree.get_root(), &low_leaf_proof),
+        tree.verify_proof(
+            &leaves[low_leaf_idx],
+            low_leaf_idx,
+            &tree.get_root(),
+            &low_leaf_proof
+        ),
         true
     );
 
-    let new_low_leaf = updated_idx_leaves[low_leaf_idx];
+    let new_low_leaf = updated_idx_leaves[low_leaf_idx].clone();
     native_hasher.update(&[
         new_low_leaf.val,
         new_low_leaf.next_val,
@@ -152,22 +163,35 @@ fn generate_state_transition_circuit_inputs(
     tree = IndexedMerkleTree::<Fr, T, RATE>::new(&mut native_hasher, leaves.clone()).unwrap();
     let (new_leaf_proof, new_leaf_proof_helper) = tree.get_proof(round as usize);
     assert_eq!(
-        tree.verify_proof(&leaves[round as usize], round as usize, &tree.get_root(), &new_leaf_proof),
+        tree.verify_proof(
+            &leaves[round as usize],
+            round as usize,
+            &tree.get_root(),
+            &new_leaf_proof
+        ),
         true
     );
 
-    native_hasher.update(&[updated_idx_leaves[round as usize].val, updated_idx_leaves[round as usize].next_idx,updated_idx_leaves[round as usize].next_val]);
+    native_hasher.update(&[
+        updated_idx_leaves[round as usize].val,
+        updated_idx_leaves[round as usize].next_idx,
+        updated_idx_leaves[round as usize].next_val,
+    ]);
     leaves[round as usize] = native_hasher.squeeze_and_reset();
     tree = IndexedMerkleTree::<Fr, T, RATE>::new(&mut native_hasher, leaves.clone()).unwrap();
 
     let new_root = tree.get_root();
     let new_leaf = IMTLeaf::<Fr> {
         val: updated_idx_leaves[round as usize].val,
-        next_val:updated_idx_leaves[round as usize].next_idx,
-        next_idx:updated_idx_leaves[round as usize].next_val,
+        next_val: updated_idx_leaves[round as usize].next_idx,
+        next_idx: updated_idx_leaves[round as usize].next_val,
     };
     let new_leaf_index = Fr::from(round);
-    let is_new_leaf_largest = if new_leaf.next_val==Fr::zero(){Fr::one()}else {Fr::zero()};
+    let is_new_leaf_largest = if new_leaf.next_val == Fr::zero() {
+        Fr::one()
+    } else {
+        Fr::zero()
+    };
 
     let idx_input = IndexTreeInput::new(
         old_root,
@@ -190,7 +214,7 @@ fn generate_state_transition_circuit_inputs(
         nullifier_affine,
     );
 
-    input
+    (input, leaves)
 }
 
 pub(crate) fn generate_wrapper_circuit_input(
@@ -257,6 +281,13 @@ pub(crate) fn generate_wrapper_circuit_input(
 
     let mut voter_input: VoterCircuitInput<Fr> = generate_random_voter_circuit_inputs();
     let mut state_input: StateTranInput<Fr> = generate_random_state_transition_circuit_inputs();
+    let mut idx_node_leaves = (0..8)
+        .map(|_| IMTLeaf::<Fr> {
+            val: Fr::from(0u64),
+            next_val: Fr::from(0u64),
+            next_idx: Fr::from(0u64),
+        })
+        .collect::<Vec<_>>();
 
     for i in 0..num_round {
         let (nullifier, s, c) = gen_test_nullifier(&sk[i], &[1u8, 0u8]);
@@ -293,11 +324,14 @@ pub(crate) fn generate_wrapper_circuit_input(
             ));
         }
 
-        state_input = generate_state_transition_circuit_inputs(
+        (state_input, leaves) = generate_state_transition_circuit_inputs(
             pk_enc.clone(),
             nullifier,
             vote_enc.clone(),
             prev_vote.clone(),
+            idx_node_leaves.clone(),
+            i as u64,
+            leaves,
         );
 
         prev_vote = prev_vote
@@ -313,84 +347,4 @@ pub(crate) fn generate_wrapper_circuit_input(
     }
 
     (voter_input, state_input)
-}
-
-#[test]
-fn test_update_idx_leaf() {
-    let mut leaves = (0..8)
-        .map(|_| IMTLeaf::<Fr> {
-            val: Fr::from(0u64),
-            next_val: Fr::from(0u64),
-            next_idx: Fr::from(0u64),
-        })
-        .collect::<Vec<_>>();
-
-    leaves = update_idx_leaf(leaves, Fr::from(30), 1);
-
-    println!("--------------test 1 -------------");
-
-    println!("leave 1 ={:?}", leaves[0].val);
-    println!("leave 1 ={:?}", leaves[0].next_idx);
-    println!("leave 1 ={:?}\n", leaves[0].next_val);
-
-    println!("leave 2 ={:?}", leaves[1].val);
-    println!("leave 2 ={:?}", leaves[1].next_idx);
-    println!("leave 2 ={:?}\n", leaves[1].next_val);
-
-    leaves = update_idx_leaf(leaves, Fr::from(10), 2);
-    println!("--------------test 2 -------------");
-
-    println!("leave 1 ={:?}", leaves[0].val);
-    println!("leave 1 ={:?}", leaves[0].next_idx);
-    println!("leave 1 ={:?}\n", leaves[0].next_val);
-
-    println!("leave 2 ={:?}", leaves[1].val);
-    println!("leave 2 ={:?}", leaves[1].next_idx);
-    println!("leave 2 ={:?}\n", leaves[1].next_val);
-
-    println!("leave 3 ={:?}", leaves[2].val);
-    println!("leave 3 ={:?}", leaves[2].next_idx);
-    println!("leave 3 ={:?}\n", leaves[2].next_val);
-
-    leaves = update_idx_leaf(leaves, Fr::from(20), 3);
-    println!("--------------test 3 -------------");
-
-    println!("leave 1 ={:?}", leaves[0].val);
-    println!("leave 1 ={:?}", leaves[0].next_idx);
-    println!("leave 1 ={:?}\n", leaves[0].next_val);
-
-    println!("leave 2 ={:?}", leaves[1].val);
-    println!("leave 2 ={:?}", leaves[1].next_idx);
-    println!("leave 2 ={:?}\n", leaves[1].next_val);
-
-    println!("leave 3 ={:?}", leaves[2].val);
-    println!("leave 3 ={:?}", leaves[2].next_idx);
-    println!("leave 3 ={:?}\n", leaves[2].next_val);
-
-    println!("leave 4 ={:?}", leaves[3].val);
-    println!("leave 4 ={:?}", leaves[3].next_idx);
-    println!("leave 4 ={:?}\n", leaves[3].next_val);
-
-    leaves = update_idx_leaf(leaves, Fr::from(50), 4);
-    println!("--------------test 4 -------------");
-
-    println!("leave 1 ={:?}", leaves[0].val);
-    println!("leave 1 ={:?}", leaves[0].next_idx);
-    println!("leave 1 ={:?}\n", leaves[0].next_val);
-
-    println!("leave 2 ={:?}", leaves[1].val);
-    println!("leave 2 ={:?}", leaves[1].next_idx);
-    println!("leave 2 ={:?}\n", leaves[1].next_val);
-
-    println!("leave 3 ={:?}", leaves[2].val);
-    println!("leave 3 ={:?}", leaves[2].next_idx);
-    println!("leave 3 ={:?}\n", leaves[2].next_val);
-
-    println!("leave 4 ={:?}", leaves[3].val);
-    println!("leave 4 ={:?}", leaves[3].next_idx);
-    println!("leave 4 ={:?}\n", leaves[3].next_val);
-
-    println!("leave 5 ={:?}", leaves[4].val);
-    println!("leave 5 ={:?}", leaves[4].next_idx);
-    println!("leave 5 ={:?}\n", leaves[4].next_val);
 }
